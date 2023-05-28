1f50d46032a669d41995620918c412dd6ee800d9
Gary Griffaw <ggriffaw@yahoo.com>
2023-05-28 14:44:52 -0500

    Add web.fn getBeerById handler and route

--- src/main/java/guru/springframework/spring6reactivemongo/web/fn/BeerHandler.java
+++ src/main/java/guru/springframework/spring6reactivemongo/web/fn/BeerHandler.java
@@ -17,4 +17,10 @@ public class BeerHandler {
         return ServerResponse.ok()
                 .body(beerService.listBeers(), BeerDTO.class);
     }
+
+    public Mono<ServerResponse> getBeerById(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.getById(request.pathVariable("beerId")), BeerDTO.class);
+
+    }
 }
